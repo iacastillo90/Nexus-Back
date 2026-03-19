76e61ada44cf8319d704fbd09dde6a44b283460b
IvanCastillo <iacastillo.ili90@gmail.com>
2026-03-19 07:33:13 -0300

    Actualizar lógica del backend

---
+++ back/src/db/migrations/20250101000015-create-tags.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('tags', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4
+            },
+            name: {
+                type: Sequelize.STRING(50),
+                allowNull: false,
+                unique: true
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+            }
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('tags');
+    }
+};
+
