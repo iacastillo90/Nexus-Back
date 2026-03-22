a7a9a1d82c3097e06e42e0a5bc759e032f5a4c35
IvanCastillo <iacastillo.ili90@gmail.com>
2026-03-22 14:14:13 -0300

    Actualizar modelos de la base de datos

---
+++ back/src/db/migrations/20250101000012-create-blocks.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('blocks', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4
+            },
+            blocker_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            },
+            blocked_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+            }
+        });
+
+        await queryInterface.addIndex('blocks', ['blocker_id', 'blocked_id'], { unique: true });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('blocks');
+    }
+};
+
