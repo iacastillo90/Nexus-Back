9558fbfbc45ea0963d74b07737be3549defd928f
IvanCastillo <iacastillo.ili90@gmail.com>
2026-04-26 04:12:49 -0400

    Actualizar gestión de estado

---
+++ app_mobile/lib/features/auth/domain/entities/auth_response_entity.freezed.dart
@@ -0,0 +1,205 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'auth_response_entity.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$AuthResponseEntity {
+  /// Token JWT (JSON Web Token) válido.
+  /// Debe enviarse en el header `Authorization: Bearer <token>` en requests protegidos.
+  String? get token => throw _privateConstructorUsedError;
+
+  /// Entidad completa del usuario autenticado.
+  UserEntity get user => throw _privateConstructorUsedError;
+
+  /// Token de larga duración para renovar el acceso sin re-login.
+  /// (Reservado para Fase 2).
+  String? get refreshToken => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $AuthResponseEntityCopyWith<AuthResponseEntity> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AuthResponseEntityCopyWith<$Res> {
+  factory $AuthResponseEntityCopyWith(
+          AuthResponseEntity value, $Res Function(AuthResponseEntity) then) =
+      _$AuthResponseEntityCopyWithImpl<$Res, AuthResponseEntity>;
+  @useResult
+  $Res call({String? token, UserEntity user, String? refreshToken});
+
+  $UserEntityCopyWith<$Res> get user;
+}
+
+/// @nodoc
+class _$AuthResponseEntityCopyWithImpl<$Res, $Val extends AuthResponseEntity>
+    implements $AuthResponseEntityCopyWith<$Res> {
+  _$AuthResponseEntityCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? token = freezed,
+    Object? user = null,
+    Object? refreshToken = freezed,
+  }) {
+    return _then(_value.copyWith(
+      token: freezed == token
+          ? _value.token
+          : token // ignore: cast_nullable_to_non_nullable
+              as String?,
+      user: null == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as UserEntity,
+      refreshToken: freezed == refreshToken
+          ? _value.refreshToken
+          : refreshToken // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $UserEntityCopyWith<$Res> get user {
+    return $UserEntityCopyWith<$Res>(_value.user, (value) {
+      return _then(_value.copyWith(user: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$AuthResponseEntityImplCopyWith<$Res>
+    implements $AuthResponseEntityCopyWith<$Res> {
+  factory _$$AuthResponseEntityImplCopyWith(_$AuthResponseEntityImpl value,
+          $Res Function(_$AuthResponseEntityImpl) then) =
+      __$$AuthResponseEntityImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String? token, UserEntity user, String? refreshToken});
+
+  @override
+  $UserEntityCopyWith<$Res> get user;
+}
+
+/// @nodoc
+class __$$AuthResponseEntityImplCopyWithImpl<$Res>
+    extends _$AuthResponseEntityCopyWithImpl<$Res, _$AuthResponseEntityImpl>
+    implements _$$AuthResponseEntityImplCopyWith<$Res> {
+  __$$AuthResponseEntityImplCopyWithImpl(_$AuthResponseEntityImpl _value,
+      $Res Function(_$AuthResponseEntityImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? token = freezed,
+    Object? user = null,
+    Object? refreshToken = freezed,
+  }) {
+    return _then(_$AuthResponseEntityImpl(
+      token: freezed == token
+          ? _value.token
+          : token // ignore: cast_nullable_to_non_nullable
+              as String?,
+      user: null == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as UserEntity,
+      refreshToken: freezed == refreshToken
+          ? _value.refreshToken
+          : refreshToken // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$AuthResponseEntityImpl implements _AuthResponseEntity {
+  const _$AuthResponseEntityImpl(
+      {this.token, required this.user, this.refreshToken});
+
+  /// Token JWT (JSON Web Token) válido.
+  /// Debe enviarse en el header `Authorization: Bearer <token>` en requests protegidos.
+  @override
+  final String? token;
+
+  /// Entidad completa del usuario autenticado.
+  @override
+  final UserEntity user;
+
+  /// Token de larga duración para renovar el acceso sin re-login.
+  /// (Reservado para Fase 2).
+  @override
+  final String? refreshToken;
+
+  @override
+  String toString() {
+    return 'AuthResponseEntity(token: $token, user: $user, refreshToken: $refreshToken)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$AuthResponseEntityImpl &&
+            (identical(other.token, token) || other.token == token) &&
+            (identical(other.user, user) || other.user == user) &&
+            (identical(other.refreshToken, refreshToken) ||
+                other.refreshToken == refreshToken));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, token, user, refreshToken);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$AuthResponseEntityImplCopyWith<_$AuthResponseEntityImpl> get copyWith =>
+      __$$AuthResponseEntityImplCopyWithImpl<_$AuthResponseEntityImpl>(
+          this, _$identity);
+}
+
+abstract class _AuthResponseEntity implements AuthResponseEntity {
+  const factory _AuthResponseEntity(
+      {final String? token,
+      required final UserEntity user,
+      final String? refreshToken}) = _$AuthResponseEntityImpl;
+
+  @override
+
+  /// Token JWT (JSON Web Token) válido.
+  /// Debe enviarse en el header `Authorization: Bearer <token>` en requests protegidos.
+  String? get token;
+  @override
+
+  /// Entidad completa del usuario autenticado.
+  UserEntity get user;
+  @override
+
+  /// Token de larga duración para renovar el acceso sin re-login.
+  /// (Reservado para Fase 2).
+  String? get refreshToken;
+  @override
+  @JsonKey(ignore: true)
+  _$$AuthResponseEntityImplCopyWith<_$AuthResponseEntityImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
