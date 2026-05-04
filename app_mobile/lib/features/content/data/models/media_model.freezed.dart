0160f1f63caf8aae9f893b2f263f210347408c65
IvanCastillo <iacastillo.ili90@gmail.com>
2026-05-04 16:27:01 -0400

    Actualizar gestión de estado

---
+++ app_mobile/lib/features/content/data/models/media_model.freezed.dart
@@ -0,0 +1,280 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'media_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+MediaModel _$MediaModelFromJson(Map<String, dynamic> json) {
+  return _MediaModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$MediaModel {
+  String get url => throw _privateConstructorUsedError;
+  String get type => throw _privateConstructorUsedError;
+  String? get thumbnailUrl => throw _privateConstructorUsedError;
+  int? get width => throw _privateConstructorUsedError;
+  int? get height => throw _privateConstructorUsedError;
+  int? get duration => throw _privateConstructorUsedError;
+  int? get size => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $MediaModelCopyWith<MediaModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MediaModelCopyWith<$Res> {
+  factory $MediaModelCopyWith(
+          MediaModel value, $Res Function(MediaModel) then) =
+      _$MediaModelCopyWithImpl<$Res, MediaModel>;
+  @useResult
+  $Res call(
+      {String url,
+      String type,
+      String? thumbnailUrl,
+      int? width,
+      int? height,
+      int? duration,
+      int? size});
+}
+
+/// @nodoc
+class _$MediaModelCopyWithImpl<$Res, $Val extends MediaModel>
+    implements $MediaModelCopyWith<$Res> {
+  _$MediaModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? url = null,
+    Object? type = null,
+    Object? thumbnailUrl = freezed,
+    Object? width = freezed,
+    Object? height = freezed,
+    Object? duration = freezed,
+    Object? size = freezed,
+  }) {
+    return _then(_value.copyWith(
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String,
+      type: null == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String,
+      thumbnailUrl: freezed == thumbnailUrl
+          ? _value.thumbnailUrl
+          : thumbnailUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      width: freezed == width
+          ? _value.width
+          : width // ignore: cast_nullable_to_non_nullable
+              as int?,
+      height: freezed == height
+          ? _value.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as int?,
+      duration: freezed == duration
+          ? _value.duration
+          : duration // ignore: cast_nullable_to_non_nullable
+              as int?,
+      size: freezed == size
+          ? _value.size
+          : size // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$MediaModelImplCopyWith<$Res>
+    implements $MediaModelCopyWith<$Res> {
+  factory _$$MediaModelImplCopyWith(
+          _$MediaModelImpl value, $Res Function(_$MediaModelImpl) then) =
+      __$$MediaModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String url,
+      String type,
+      String? thumbnailUrl,
+      int? width,
+      int? height,
+      int? duration,
+      int? size});
+}
+
+/// @nodoc
+class __$$MediaModelImplCopyWithImpl<$Res>
+    extends _$MediaModelCopyWithImpl<$Res, _$MediaModelImpl>
+    implements _$$MediaModelImplCopyWith<$Res> {
+  __$$MediaModelImplCopyWithImpl(
+      _$MediaModelImpl _value, $Res Function(_$MediaModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? url = null,
+    Object? type = null,
+    Object? thumbnailUrl = freezed,
+    Object? width = freezed,
+    Object? height = freezed,
+    Object? duration = freezed,
+    Object? size = freezed,
+  }) {
+    return _then(_$MediaModelImpl(
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String,
+      type: null == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String,
+      thumbnailUrl: freezed == thumbnailUrl
+          ? _value.thumbnailUrl
+          : thumbnailUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      width: freezed == width
+          ? _value.width
+          : width // ignore: cast_nullable_to_non_nullable
+              as int?,
+      height: freezed == height
+          ? _value.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as int?,
+      duration: freezed == duration
+          ? _value.duration
+          : duration // ignore: cast_nullable_to_non_nullable
+              as int?,
+      size: freezed == size
+          ? _value.size
+          : size // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$MediaModelImpl extends _MediaModel {
+  const _$MediaModelImpl(
+      {required this.url,
+      required this.type,
+      this.thumbnailUrl,
+      this.width,
+      this.height,
+      this.duration,
+      this.size})
+      : super._();
+
+  factory _$MediaModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$MediaModelImplFromJson(json);
+
+  @override
+  final String url;
+  @override
+  final String type;
+  @override
+  final String? thumbnailUrl;
+  @override
+  final int? width;
+  @override
+  final int? height;
+  @override
+  final int? duration;
+  @override
+  final int? size;
+
+  @override
+  String toString() {
+    return 'MediaModel(url: $url, type: $type, thumbnailUrl: $thumbnailUrl, width: $width, height: $height, duration: $duration, size: $size)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$MediaModelImpl &&
+            (identical(other.url, url) || other.url == url) &&
+            (identical(other.type, type) || other.type == type) &&
+            (identical(other.thumbnailUrl, thumbnailUrl) ||
+                other.thumbnailUrl == thumbnailUrl) &&
+            (identical(other.width, width) || other.width == width) &&
+            (identical(other.height, height) || other.height == height) &&
+            (identical(other.duration, duration) ||
+                other.duration == duration) &&
+            (identical(other.size, size) || other.size == size));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, url, type, thumbnailUrl, width, height, duration, size);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$MediaModelImplCopyWith<_$MediaModelImpl> get copyWith =>
+      __$$MediaModelImplCopyWithImpl<_$MediaModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$MediaModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _MediaModel extends MediaModel {
+  const factory _MediaModel(
+      {required final String url,
+      required final String type,
+      final String? thumbnailUrl,
+      final int? width,
+      final int? height,
+      final int? duration,
+      final int? size}) = _$MediaModelImpl;
+  const _MediaModel._() : super._();
+
+  factory _MediaModel.fromJson(Map<String, dynamic> json) =
+      _$MediaModelImpl.fromJson;
+
+  @override
+  String get url;
+  @override
+  String get type;
+  @override
+  String? get thumbnailUrl;
+  @override
+  int? get width;
+  @override
+  int? get height;
+  @override
+  int? get duration;
+  @override
+  int? get size;
+  @override
+  @JsonKey(ignore: true)
+  _$$MediaModelImplCopyWith<_$MediaModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
