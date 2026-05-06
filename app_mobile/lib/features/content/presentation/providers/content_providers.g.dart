9be101a0aa3f7e535ea4f918dcb5bc899f054cbd
IvanCastillo <iacastillo.ili90@gmail.com>
2026-05-06 08:00:25 -0400

    Actualizar gestión de estado

---
+++ app_mobile/lib/features/content/presentation/providers/content_providers.g.dart
@@ -0,0 +1,87 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'content_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$mediaUploadDataSourceHash() =>
+    r'5933751a4b4ca5b753dcbf93e7e2f86265b642c5';
+
+/// Media upload datasource provider
+///
+/// Copied from [mediaUploadDataSource].
+@ProviderFor(mediaUploadDataSource)
+final mediaUploadDataSourceProvider =
+    AutoDisposeProvider<MediaUploadDataSource>.internal(
+  mediaUploadDataSource,
+  name: r'mediaUploadDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$mediaUploadDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef MediaUploadDataSourceRef
+    = AutoDisposeProviderRef<MediaUploadDataSource>;
+String _$postCreationDataSourceHash() =>
+    r'e432e89dc9017a14f4e1c693deccb6217b05c8cf';
+
+/// Post creation datasource provider
+///
+/// Copied from [postCreationDataSource].
+@ProviderFor(postCreationDataSource)
+final postCreationDataSourceProvider =
+    AutoDisposeProvider<PostCreationDataSource>.internal(
+  postCreationDataSource,
+  name: r'postCreationDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$postCreationDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PostCreationDataSourceRef
+    = AutoDisposeProviderRef<PostCreationDataSource>;
+String _$contentRepositoryHash() => r'754439d885728c62f9692e4d24031a4e4452715f';
+
+/// Content repository provider
+///
+/// Copied from [contentRepository].
+@ProviderFor(contentRepository)
+final contentRepositoryProvider =
+    AutoDisposeProvider<ContentRepository>.internal(
+  contentRepository,
+  name: r'contentRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$contentRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ContentRepositoryRef = AutoDisposeProviderRef<ContentRepository>;
+String _$createPostControllerHash() =>
+    r'b93b0126f5ef970e1d672b22b222bf3f568ee87a';
+
+/// Create post controller provider
+///
+/// Copied from [CreatePostController].
+@ProviderFor(CreatePostController)
+final createPostControllerProvider =
+    AutoDisposeNotifierProvider<CreatePostController, CreatePostState>.internal(
+  CreatePostController.new,
+  name: r'createPostControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$createPostControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CreatePostController = AutoDisposeNotifier<CreatePostState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
