a699ba1bcf0d51609d39f16dac7e54dc1246d1fc
IvanCastillo <iacastillo.ili90@gmail.com>
2026-05-08 07:52:01 -0400

    Implementar funcionalidad en Flutter

---
+++ app_mobile/lib/features/dreams/data/models/dream_model.freezed.dart
@@ -0,0 +1,452 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'dream_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+DreamModel _$DreamModelFromJson(Map<String, dynamic> json) {
+  return _DreamModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$DreamModel {
+  String get id => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  String get creatorId => throw _privateConstructorUsedError;
+  String get creatorName => throw _privateConstructorUsedError;
+  String? get creatorAvatar => throw _privateConstructorUsedError;
+  int get maxContributions => throw _privateConstructorUsedError;
+  int get currentContributions => throw _privateConstructorUsedError;
+  String get status => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
+  DateTime? get completedAt => throw _privateConstructorUsedError;
+  List<String> get tags => throw _privateConstructorUsedError;
+  int get viewCount => throw _privateConstructorUsedError;
+  int get likeCount => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $DreamModelCopyWith<DreamModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DreamModelCopyWith<$Res> {
+  factory $DreamModelCopyWith(
+          DreamModel value, $Res Function(DreamModel) then) =
+      _$DreamModelCopyWithImpl<$Res, DreamModel>;
+  @useResult
+  $Res call(
+      {String id,
+      String title,
+      String description,
+      String creatorId,
+      String creatorName,
+      String? creatorAvatar,
+      int maxContributions,
+      int currentContributions,
+      String status,
+      DateTime createdAt,
+      DateTime? completedAt,
+      List<String> tags,
+      int viewCount,
+      int likeCount});
+}
+
+/// @nodoc
+class _$DreamModelCopyWithImpl<$Res, $Val extends DreamModel>
+    implements $DreamModelCopyWith<$Res> {
+  _$DreamModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? title = null,
+    Object? description = null,
+    Object? creatorId = null,
+    Object? creatorName = null,
+    Object? creatorAvatar = freezed,
+    Object? maxContributions = null,
+    Object? currentContributions = null,
+    Object? status = null,
+    Object? createdAt = null,
+    Object? completedAt = freezed,
+    Object? tags = null,
+    Object? viewCount = null,
+    Object? likeCount = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      creatorId: null == creatorId
+          ? _value.creatorId
+          : creatorId // ignore: cast_nullable_to_non_nullable
+              as String,
+      creatorName: null == creatorName
+          ? _value.creatorName
+          : creatorName // ignore: cast_nullable_to_non_nullable
+              as String,
+      creatorAvatar: freezed == creatorAvatar
+          ? _value.creatorAvatar
+          : creatorAvatar // ignore: cast_nullable_to_non_nullable
+              as String?,
+      maxContributions: null == maxContributions
+          ? _value.maxContributions
+          : maxContributions // ignore: cast_nullable_to_non_nullable
+              as int,
+      currentContributions: null == currentContributions
+          ? _value.currentContributions
+          : currentContributions // ignore: cast_nullable_to_non_nullable
+              as int,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      completedAt: freezed == completedAt
+          ? _value.completedAt
+          : completedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      tags: null == tags
+          ? _value.tags
+          : tags // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      viewCount: null == viewCount
+          ? _value.viewCount
+          : viewCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      likeCount: null == likeCount
+          ? _value.likeCount
+          : likeCount // ignore: cast_nullable_to_non_nullable
+              as int,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$DreamModelImplCopyWith<$Res>
+    implements $DreamModelCopyWith<$Res> {
+  factory _$$DreamModelImplCopyWith(
+          _$DreamModelImpl value, $Res Function(_$DreamModelImpl) then) =
+      __$$DreamModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      String title,
+      String description,
+      String creatorId,
+      String creatorName,
+      String? creatorAvatar,
+      int maxContributions,
+      int currentContributions,
+      String status,
+      DateTime createdAt,
+      DateTime? completedAt,
+      List<String> tags,
+      int viewCount,
+      int likeCount});
+}
+
+/// @nodoc
+class __$$DreamModelImplCopyWithImpl<$Res>
+    extends _$DreamModelCopyWithImpl<$Res, _$DreamModelImpl>
+    implements _$$DreamModelImplCopyWith<$Res> {
+  __$$DreamModelImplCopyWithImpl(
+      _$DreamModelImpl _value, $Res Function(_$DreamModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? title = null,
+    Object? description = null,
+    Object? creatorId = null,
+    Object? creatorName = null,
+    Object? creatorAvatar = freezed,
+    Object? maxContributions = null,
+    Object? currentContributions = null,
+    Object? status = null,
+    Object? createdAt = null,
+    Object? completedAt = freezed,
+    Object? tags = null,
+    Object? viewCount = null,
+    Object? likeCount = null,
+  }) {
+    return _then(_$DreamModelImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      creatorId: null == creatorId
+          ? _value.creatorId
+          : creatorId // ignore: cast_nullable_to_non_nullable
+              as String,
+      creatorName: null == creatorName
+          ? _value.creatorName
+          : creatorName // ignore: cast_nullable_to_non_nullable
+              as String,
+      creatorAvatar: freezed == creatorAvatar
+          ? _value.creatorAvatar
+          : creatorAvatar // ignore: cast_nullable_to_non_nullable
+              as String?,
+      maxContributions: null == maxContributions
+          ? _value.maxContributions
+          : maxContributions // ignore: cast_nullable_to_non_nullable
+              as int,
+      currentContributions: null == currentContributions
+          ? _value.currentContributions
+          : currentContributions // ignore: cast_nullable_to_non_nullable
+              as int,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      completedAt: freezed == completedAt
+          ? _value.completedAt
+          : completedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      tags: null == tags
+          ? _value._tags
+          : tags // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      viewCount: null == viewCount
+          ? _value.viewCount
+          : viewCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      likeCount: null == likeCount
+          ? _value.likeCount
+          : likeCount // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$DreamModelImpl extends _DreamModel {
+  const _$DreamModelImpl(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.creatorId,
+      required this.creatorName,
+      this.creatorAvatar,
+      required this.maxContributions,
+      required this.currentContributions,
+      required this.status,
+      required this.createdAt,
+      this.completedAt,
+      final List<String> tags = const [],
+      this.viewCount = 0,
+      this.likeCount = 0})
+      : _tags = tags,
+        super._();
+
+  factory _$DreamModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$DreamModelImplFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String title;
+  @override
+  final String description;
+  @override
+  final String creatorId;
+  @override
+  final String creatorName;
+  @override
+  final String? creatorAvatar;
+  @override
+  final int maxContributions;
+  @override
+  final int currentContributions;
+  @override
+  final String status;
+  @override
+  final DateTime createdAt;
+  @override
+  final DateTime? completedAt;
+  final List<String> _tags;
+  @override
+  @JsonKey()
+  List<String> get tags {
+    if (_tags is EqualUnmodifiableListView) return _tags;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_tags);
+  }
+
+  @override
+  @JsonKey()
+  final int viewCount;
+  @override
+  @JsonKey()
+  final int likeCount;
+
+  @override
+  String toString() {
+    return 'DreamModel(id: $id, title: $title, description: $description, creatorId: $creatorId, creatorName: $creatorName, creatorAvatar: $creatorAvatar, maxContributions: $maxContributions, currentContributions: $currentContributions, status: $status, createdAt: $createdAt, completedAt: $completedAt, tags: $tags, viewCount: $viewCount, likeCount: $likeCount)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$DreamModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.creatorId, creatorId) ||
+                other.creatorId == creatorId) &&
+            (identical(other.creatorName, creatorName) ||
+                other.creatorName == creatorName) &&
+            (identical(other.creatorAvatar, creatorAvatar) ||
+                other.creatorAvatar == creatorAvatar) &&
+            (identical(other.maxContributions, maxContributions) ||
+                other.maxContributions == maxContributions) &&
+            (identical(other.currentContributions, currentContributions) ||
+                other.currentContributions == currentContributions) &&
+            (identical(other.status, status) || other.status == status) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.completedAt, completedAt) ||
+                other.completedAt == completedAt) &&
+            const DeepCollectionEquality().equals(other._tags, _tags) &&
+            (identical(other.viewCount, viewCount) ||
+                other.viewCount == viewCount) &&
+            (identical(other.likeCount, likeCount) ||
+                other.likeCount == likeCount));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      title,
+      description,
+      creatorId,
+      creatorName,
+      creatorAvatar,
+      maxContributions,
+      currentContributions,
+      status,
+      createdAt,
+      completedAt,
+      const DeepCollectionEquality().hash(_tags),
+      viewCount,
+      likeCount);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$DreamModelImplCopyWith<_$DreamModelImpl> get copyWith =>
+      __$$DreamModelImplCopyWithImpl<_$DreamModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$DreamModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _DreamModel extends DreamModel {
+  const factory _DreamModel(
+      {required final String id,
+      required final String title,
+      required final String description,
+      required final String creatorId,
+      required final String creatorName,
+      final String? creatorAvatar,
+      required final int maxContributions,
+      required final int currentContributions,
+      required final String status,
+      required final DateTime createdAt,
+      final DateTime? completedAt,
+      final List<String> tags,
+      final int viewCount,
+      final int likeCount}) = _$DreamModelImpl;
+  const _DreamModel._() : super._();
+
+  factory _DreamModel.fromJson(Map<String, dynamic> json) =
+      _$DreamModelImpl.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get title;
+  @override
+  String get description;
+  @override
+  String get creatorId;
+  @override
+  String get creatorName;
+  @override
+  String? get creatorAvatar;
+  @override
+  int get maxContributions;
+  @override
+  int get currentContributions;
+  @override
+  String get status;
+  @override
+  DateTime get createdAt;
+  @override
+  DateTime? get completedAt;
+  @override
+  List<String> get tags;
+  @override
+  int get viewCount;
+  @override
+  int get likeCount;
+  @override
+  @JsonKey(ignore: true)
+  _$$DreamModelImplCopyWith<_$DreamModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
