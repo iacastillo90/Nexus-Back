46a3e7edca998b93cd20788b2fa2a20d68449c9b
IvanCastillo <iacastillo.ili90@gmail.com>
2026-05-18 11:51:49 -0400

    Actualizar UI móvil

---
+++ app_mobile/lib/features/profile/data/models/karma_model.freezed.dart
@@ -0,0 +1,364 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'karma_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+KarmaModel _$KarmaModelFromJson(Map<String, dynamic> json) {
+  return _KarmaModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$KarmaModel {
+  int get totalScore => throw _privateConstructorUsedError;
+  int get authenticityScore => throw _privateConstructorUsedError;
+  int get contributionScore => throw _privateConstructorUsedError;
+  int get communityScore => throw _privateConstructorUsedError;
+  int get consistencyScore => throw _privateConstructorUsedError;
+  String get tier => throw _privateConstructorUsedError;
+  double get tierProgress => throw _privateConstructorUsedError;
+  List<String> get privileges => throw _privateConstructorUsedError;
+  int? get rank => throw _privateConstructorUsedError;
+  int? get weeklyChange => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $KarmaModelCopyWith<KarmaModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $KarmaModelCopyWith<$Res> {
+  factory $KarmaModelCopyWith(
+          KarmaModel value, $Res Function(KarmaModel) then) =
+      _$KarmaModelCopyWithImpl<$Res, KarmaModel>;
+  @useResult
+  $Res call(
+      {int totalScore,
+      int authenticityScore,
+      int contributionScore,
+      int communityScore,
+      int consistencyScore,
+      String tier,
+      double tierProgress,
+      List<String> privileges,
+      int? rank,
+      int? weeklyChange});
+}
+
+/// @nodoc
+class _$KarmaModelCopyWithImpl<$Res, $Val extends KarmaModel>
+    implements $KarmaModelCopyWith<$Res> {
+  _$KarmaModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? totalScore = null,
+    Object? authenticityScore = null,
+    Object? contributionScore = null,
+    Object? communityScore = null,
+    Object? consistencyScore = null,
+    Object? tier = null,
+    Object? tierProgress = null,
+    Object? privileges = null,
+    Object? rank = freezed,
+    Object? weeklyChange = freezed,
+  }) {
+    return _then(_value.copyWith(
+      totalScore: null == totalScore
+          ? _value.totalScore
+          : totalScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      authenticityScore: null == authenticityScore
+          ? _value.authenticityScore
+          : authenticityScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      contributionScore: null == contributionScore
+          ? _value.contributionScore
+          : contributionScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      communityScore: null == communityScore
+          ? _value.communityScore
+          : communityScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      consistencyScore: null == consistencyScore
+          ? _value.consistencyScore
+          : consistencyScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      tier: null == tier
+          ? _value.tier
+          : tier // ignore: cast_nullable_to_non_nullable
+              as String,
+      tierProgress: null == tierProgress
+          ? _value.tierProgress
+          : tierProgress // ignore: cast_nullable_to_non_nullable
+              as double,
+      privileges: null == privileges
+          ? _value.privileges
+          : privileges // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      rank: freezed == rank
+          ? _value.rank
+          : rank // ignore: cast_nullable_to_non_nullable
+              as int?,
+      weeklyChange: freezed == weeklyChange
+          ? _value.weeklyChange
+          : weeklyChange // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$KarmaModelImplCopyWith<$Res>
+    implements $KarmaModelCopyWith<$Res> {
+  factory _$$KarmaModelImplCopyWith(
+          _$KarmaModelImpl value, $Res Function(_$KarmaModelImpl) then) =
+      __$$KarmaModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int totalScore,
+      int authenticityScore,
+      int contributionScore,
+      int communityScore,
+      int consistencyScore,
+      String tier,
+      double tierProgress,
+      List<String> privileges,
+      int? rank,
+      int? weeklyChange});
+}
+
+/// @nodoc
+class __$$KarmaModelImplCopyWithImpl<$Res>
+    extends _$KarmaModelCopyWithImpl<$Res, _$KarmaModelImpl>
+    implements _$$KarmaModelImplCopyWith<$Res> {
+  __$$KarmaModelImplCopyWithImpl(
+      _$KarmaModelImpl _value, $Res Function(_$KarmaModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? totalScore = null,
+    Object? authenticityScore = null,
+    Object? contributionScore = null,
+    Object? communityScore = null,
+    Object? consistencyScore = null,
+    Object? tier = null,
+    Object? tierProgress = null,
+    Object? privileges = null,
+    Object? rank = freezed,
+    Object? weeklyChange = freezed,
+  }) {
+    return _then(_$KarmaModelImpl(
+      totalScore: null == totalScore
+          ? _value.totalScore
+          : totalScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      authenticityScore: null == authenticityScore
+          ? _value.authenticityScore
+          : authenticityScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      contributionScore: null == contributionScore
+          ? _value.contributionScore
+          : contributionScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      communityScore: null == communityScore
+          ? _value.communityScore
+          : communityScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      consistencyScore: null == consistencyScore
+          ? _value.consistencyScore
+          : consistencyScore // ignore: cast_nullable_to_non_nullable
+              as int,
+      tier: null == tier
+          ? _value.tier
+          : tier // ignore: cast_nullable_to_non_nullable
+              as String,
+      tierProgress: null == tierProgress
+          ? _value.tierProgress
+          : tierProgress // ignore: cast_nullable_to_non_nullable
+              as double,
+      privileges: null == privileges
+          ? _value._privileges
+          : privileges // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      rank: freezed == rank
+          ? _value.rank
+          : rank // ignore: cast_nullable_to_non_nullable
+              as int?,
+      weeklyChange: freezed == weeklyChange
+          ? _value.weeklyChange
+          : weeklyChange // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$KarmaModelImpl extends _KarmaModel {
+  const _$KarmaModelImpl(
+      {required this.totalScore,
+      required this.authenticityScore,
+      required this.contributionScore,
+      required this.communityScore,
+      required this.consistencyScore,
+      required this.tier,
+      required this.tierProgress,
+      final List<String> privileges = const [],
+      this.rank,
+      this.weeklyChange})
+      : _privileges = privileges,
+        super._();
+
+  factory _$KarmaModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$KarmaModelImplFromJson(json);
+
+  @override
+  final int totalScore;
+  @override
+  final int authenticityScore;
+  @override
+  final int contributionScore;
+  @override
+  final int communityScore;
+  @override
+  final int consistencyScore;
+  @override
+  final String tier;
+  @override
+  final double tierProgress;
+  final List<String> _privileges;
+  @override
+  @JsonKey()
+  List<String> get privileges {
+    if (_privileges is EqualUnmodifiableListView) return _privileges;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_privileges);
+  }
+
+  @override
+  final int? rank;
+  @override
+  final int? weeklyChange;
+
+  @override
+  String toString() {
+    return 'KarmaModel(totalScore: $totalScore, authenticityScore: $authenticityScore, contributionScore: $contributionScore, communityScore: $communityScore, consistencyScore: $consistencyScore, tier: $tier, tierProgress: $tierProgress, privileges: $privileges, rank: $rank, weeklyChange: $weeklyChange)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$KarmaModelImpl &&
+            (identical(other.totalScore, totalScore) ||
+                other.totalScore == totalScore) &&
+            (identical(other.authenticityScore, authenticityScore) ||
+                other.authenticityScore == authenticityScore) &&
+            (identical(other.contributionScore, contributionScore) ||
+                other.contributionScore == contributionScore) &&
+            (identical(other.communityScore, communityScore) ||
+                other.communityScore == communityScore) &&
+            (identical(other.consistencyScore, consistencyScore) ||
+                other.consistencyScore == consistencyScore) &&
+            (identical(other.tier, tier) || other.tier == tier) &&
+            (identical(other.tierProgress, tierProgress) ||
+                other.tierProgress == tierProgress) &&
+            const DeepCollectionEquality()
+                .equals(other._privileges, _privileges) &&
+            (identical(other.rank, rank) || other.rank == rank) &&
+            (identical(other.weeklyChange, weeklyChange) ||
+                other.weeklyChange == weeklyChange));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      totalScore,
+      authenticityScore,
+      contributionScore,
+      communityScore,
+      consistencyScore,
+      tier,
+      tierProgress,
+      const DeepCollectionEquality().hash(_privileges),
+      rank,
+      weeklyChange);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$KarmaModelImplCopyWith<_$KarmaModelImpl> get copyWith =>
+      __$$KarmaModelImplCopyWithImpl<_$KarmaModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$KarmaModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _KarmaModel extends KarmaModel {
+  const factory _KarmaModel(
+      {required final int totalScore,
+      required final int authenticityScore,
+      required final int contributionScore,
+      required final int communityScore,
+      required final int consistencyScore,
+      required final String tier,
+      required final double tierProgress,
+      final List<String> privileges,
+      final int? rank,
+      final int? weeklyChange}) = _$KarmaModelImpl;
+  const _KarmaModel._() : super._();
+
+  factory _KarmaModel.fromJson(Map<String, dynamic> json) =
+      _$KarmaModelImpl.fromJson;
+
+  @override
+  int get totalScore;
+  @override
+  int get authenticityScore;
+  @override
+  int get contributionScore;
+  @override
+  int get communityScore;
+  @override
+  int get consistencyScore;
+  @override
+  String get tier;
+  @override
+  double get tierProgress;
+  @override
+  List<String> get privileges;
+  @override
+  int? get rank;
+  @override
+  int? get weeklyChange;
+  @override
+  @JsonKey(ignore: true)
+  _$$KarmaModelImplCopyWith<_$KarmaModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
