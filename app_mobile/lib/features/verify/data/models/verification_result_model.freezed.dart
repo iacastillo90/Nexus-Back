4b18f74094e894bbdf41c54a6ac1303de4cb1281
IvanCastillo <iacastillo.ili90@gmail.com>
2026-05-28 16:01:01 -0400

    Actualizar UI móvil

---
+++ app_mobile/lib/features/verify/data/models/verification_result_model.freezed.dart
@@ -0,0 +1,348 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'verification_result_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+VerificationResultModel _$VerificationResultModelFromJson(
+    Map<String, dynamic> json) {
+  return _VerificationResultModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$VerificationResultModel {
+  String get contentId => throw _privateConstructorUsedError;
+  String get contentDNA => throw _privateConstructorUsedError;
+  String get status => throw _privateConstructorUsedError;
+  double get authenticityScore => throw _privateConstructorUsedError;
+  DateTime get verifiedAt => throw _privateConstructorUsedError;
+  String? get originalAuthor => throw _privateConstructorUsedError;
+  String? get originalSource => throw _privateConstructorUsedError;
+  DateTime? get originalDate => throw _privateConstructorUsedError;
+  List<String> get modifications => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $VerificationResultModelCopyWith<VerificationResultModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $VerificationResultModelCopyWith<$Res> {
+  factory $VerificationResultModelCopyWith(VerificationResultModel value,
+          $Res Function(VerificationResultModel) then) =
+      _$VerificationResultModelCopyWithImpl<$Res, VerificationResultModel>;
+  @useResult
+  $Res call(
+      {String contentId,
+      String contentDNA,
+      String status,
+      double authenticityScore,
+      DateTime verifiedAt,
+      String? originalAuthor,
+      String? originalSource,
+      DateTime? originalDate,
+      List<String> modifications});
+}
+
+/// @nodoc
+class _$VerificationResultModelCopyWithImpl<$Res,
+        $Val extends VerificationResultModel>
+    implements $VerificationResultModelCopyWith<$Res> {
+  _$VerificationResultModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? contentId = null,
+    Object? contentDNA = null,
+    Object? status = null,
+    Object? authenticityScore = null,
+    Object? verifiedAt = null,
+    Object? originalAuthor = freezed,
+    Object? originalSource = freezed,
+    Object? originalDate = freezed,
+    Object? modifications = null,
+  }) {
+    return _then(_value.copyWith(
+      contentId: null == contentId
+          ? _value.contentId
+          : contentId // ignore: cast_nullable_to_non_nullable
+              as String,
+      contentDNA: null == contentDNA
+          ? _value.contentDNA
+          : contentDNA // ignore: cast_nullable_to_non_nullable
+              as String,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as String,
+      authenticityScore: null == authenticityScore
+          ? _value.authenticityScore
+          : authenticityScore // ignore: cast_nullable_to_non_nullable
+              as double,
+      verifiedAt: null == verifiedAt
+          ? _value.verifiedAt
+          : verifiedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      originalAuthor: freezed == originalAuthor
+          ? _value.originalAuthor
+          : originalAuthor // ignore: cast_nullable_to_non_nullable
+              as String?,
+      originalSource: freezed == originalSource
+          ? _value.originalSource
+          : originalSource // ignore: cast_nullable_to_non_nullable
+              as String?,
+      originalDate: freezed == originalDate
+          ? _value.originalDate
+          : originalDate // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      modifications: null == modifications
+          ? _value.modifications
+          : modifications // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$VerificationResultModelImplCopyWith<$Res>
+    implements $VerificationResultModelCopyWith<$Res> {
+  factory _$$VerificationResultModelImplCopyWith(
+          _$VerificationResultModelImpl value,
+          $Res Function(_$VerificationResultModelImpl) then) =
+      __$$VerificationResultModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String contentId,
+      String contentDNA,
+      String status,
+      double authenticityScore,
+      DateTime verifiedAt,
+      String? originalAuthor,
+      String? originalSource,
+      DateTime? originalDate,
+      List<String> modifications});
+}
+
+/// @nodoc
+class __$$VerificationResultModelImplCopyWithImpl<$Res>
+    extends _$VerificationResultModelCopyWithImpl<$Res,
+        _$VerificationResultModelImpl>
+    implements _$$VerificationResultModelImplCopyWith<$Res> {
+  __$$VerificationResultModelImplCopyWithImpl(
+      _$VerificationResultModelImpl _value,
+      $Res Function(_$VerificationResultModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? contentId = null,
+    Object? contentDNA = null,
+    Object? status = null,
+    Object? authenticityScore = null,
+    Object? verifiedAt = null,
+    Object? originalAuthor = freezed,
+    Object? originalSource = freezed,
+    Object? originalDate = freezed,
+    Object? modifications = null,
+  }) {
+    return _then(_$VerificationResultModelImpl(
+      contentId: null == contentId
+          ? _value.contentId
+          : contentId // ignore: cast_nullable_to_non_nullable
+              as String,
+      contentDNA: null == contentDNA
+          ? _value.contentDNA
+          : contentDNA // ignore: cast_nullable_to_non_nullable
+              as String,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as String,
+      authenticityScore: null == authenticityScore
+          ? _value.authenticityScore
+          : authenticityScore // ignore: cast_nullable_to_non_nullable
+              as double,
+      verifiedAt: null == verifiedAt
+          ? _value.verifiedAt
+          : verifiedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      originalAuthor: freezed == originalAuthor
+          ? _value.originalAuthor
+          : originalAuthor // ignore: cast_nullable_to_non_nullable
+              as String?,
+      originalSource: freezed == originalSource
+          ? _value.originalSource
+          : originalSource // ignore: cast_nullable_to_non_nullable
+              as String?,
+      originalDate: freezed == originalDate
+          ? _value.originalDate
+          : originalDate // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      modifications: null == modifications
+          ? _value._modifications
+          : modifications // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$VerificationResultModelImpl extends _VerificationResultModel {
+  const _$VerificationResultModelImpl(
+      {required this.contentId,
+      required this.contentDNA,
+      required this.status,
+      required this.authenticityScore,
+      required this.verifiedAt,
+      this.originalAuthor,
+      this.originalSource,
+      this.originalDate,
+      final List<String> modifications = const []})
+      : _modifications = modifications,
+        super._();
+
+  factory _$VerificationResultModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$VerificationResultModelImplFromJson(json);
+
+  @override
+  final String contentId;
+  @override
+  final String contentDNA;
+  @override
+  final String status;
+  @override
+  final double authenticityScore;
+  @override
+  final DateTime verifiedAt;
+  @override
+  final String? originalAuthor;
+  @override
+  final String? originalSource;
+  @override
+  final DateTime? originalDate;
+  final List<String> _modifications;
+  @override
+  @JsonKey()
+  List<String> get modifications {
+    if (_modifications is EqualUnmodifiableListView) return _modifications;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_modifications);
+  }
+
+  @override
+  String toString() {
+    return 'VerificationResultModel(contentId: $contentId, contentDNA: $contentDNA, status: $status, authenticityScore: $authenticityScore, verifiedAt: $verifiedAt, originalAuthor: $originalAuthor, originalSource: $originalSource, originalDate: $originalDate, modifications: $modifications)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$VerificationResultModelImpl &&
+            (identical(other.contentId, contentId) ||
+                other.contentId == contentId) &&
+            (identical(other.contentDNA, contentDNA) ||
+                other.contentDNA == contentDNA) &&
+            (identical(other.status, status) || other.status == status) &&
+            (identical(other.authenticityScore, authenticityScore) ||
+                other.authenticityScore == authenticityScore) &&
+            (identical(other.verifiedAt, verifiedAt) ||
+                other.verifiedAt == verifiedAt) &&
+            (identical(other.originalAuthor, originalAuthor) ||
+                other.originalAuthor == originalAuthor) &&
+            (identical(other.originalSource, originalSource) ||
+                other.originalSource == originalSource) &&
+            (identical(other.originalDate, originalDate) ||
+                other.originalDate == originalDate) &&
+            const DeepCollectionEquality()
+                .equals(other._modifications, _modifications));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      contentId,
+      contentDNA,
+      status,
+      authenticityScore,
+      verifiedAt,
+      originalAuthor,
+      originalSource,
+      originalDate,
+      const DeepCollectionEquality().hash(_modifications));
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$VerificationResultModelImplCopyWith<_$VerificationResultModelImpl>
+      get copyWith => __$$VerificationResultModelImplCopyWithImpl<
+          _$VerificationResultModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$VerificationResultModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _VerificationResultModel extends VerificationResultModel {
+  const factory _VerificationResultModel(
+      {required final String contentId,
+      required final String contentDNA,
+      required final String status,
+      required final double authenticityScore,
+      required final DateTime verifiedAt,
+      final String? originalAuthor,
+      final String? originalSource,
+      final DateTime? originalDate,
+      final List<String> modifications}) = _$VerificationResultModelImpl;
+  const _VerificationResultModel._() : super._();
+
+  factory _VerificationResultModel.fromJson(Map<String, dynamic> json) =
+      _$VerificationResultModelImpl.fromJson;
+
+  @override
+  String get contentId;
+  @override
+  String get contentDNA;
+  @override
+  String get status;
+  @override
+  double get authenticityScore;
+  @override
+  DateTime get verifiedAt;
+  @override
+  String? get originalAuthor;
+  @override
+  String? get originalSource;
+  @override
+  DateTime? get originalDate;
+  @override
+  List<String> get modifications;
+  @override
+  @JsonKey(ignore: true)
+  _$$VerificationResultModelImplCopyWith<_$VerificationResultModelImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
