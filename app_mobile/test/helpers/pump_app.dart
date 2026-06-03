79b2855f0c31acfbf35e06bbbc776278634c061b
IvanCastillo <iacastillo.ili90@gmail.com>
2026-06-03 17:57:13 -0400

    Actualizar UI móvil

---
+++ app_mobile/test/helpers/pump_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nexus_mobile/app/theme/nexus_theme.dart';
+
+extension PumpApp on WidgetTester {
+  Future<void> pumpApp(
+    Widget widget, {
+    List<Override> overrides = const [],
+  }) {
+    return pumpWidget(
+      ProviderScope(
+        overrides: overrides,
+        child: MaterialApp(
+          theme: NexusTheme.darkTheme,
+          home: widget,
+        ),
+      ),
+    );
+  }
+}
